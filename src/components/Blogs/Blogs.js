6562d5141663d9160d4ea29aea169d1f4fb84500
Sadia-90 <sadiamahmood24@gmail.com>
2022-04-04 10:47:45 +0600

    Q and A

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,20 @@ import "./Blogs.css"
 const Blogs = () => {
     return (
         <div>
-            <h1>this is blogs</h1>
+            <h1>what is the context API</h1>
+            <p>The Context API is a React structure.I am enables to exchange unique details. And I can assists in solving prop-drilling from all levels of my application.It's do our work easy.It's breaks  our unmaintainable components and make a smaller components.  </p>
+
+            <h1>What is semantic tag?</h1>
+            <p>semantic tag elements are clearly discribed it's meaning.
+                It's can be used to define different parts of a web page.
+                It's can be used to break our page into identified parts.
+                If i use it's then i read a code page thats so much easier.It's offers us to a better user experience.
+            </p>
+            <h1> inline, block and inline block element</h1>
+
+            <p>Inline, block: Inline-block allows to set a width and height on the element. Inline-block does not add a line-break after the element. Inline-block is display list items horizontally instead of vertically.
+            Inline-block element: Inline-block element has no required attributes.There are two display values 1.block 2.inline. 
+            </p>
         </div>
     );
 };
