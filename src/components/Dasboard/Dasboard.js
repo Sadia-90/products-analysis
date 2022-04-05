51bd1c0796d49c357fd9e54fc939515c8bfd5dee
Sadia-90 <sadiamahmood24@gmail.com>
2022-04-05 12:51:57 +0600

    finish dasboard

--- src/components/Dasboard/Dasboard.js
+++ src/components/Dasboard/Dasboard.js
@@ -1,72 +1,73 @@
 import React from 'react';
+// import React { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, CartesianGrid, BarChart, Bar } from 'recharts';
 
 const Dasboard = () => {
+
+    const data = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+
     return (
         <div>
-            <h1>dasboard</h1>
+            <LineChart width={730} height={250} data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="investment" stroke="#8884d8" />
+        <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+      </LineChart>
+
+<BarChart width={730} height={250} data={data}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="month" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <Bar dataKey="investment" fill="#8884d8" />
+  <Bar dataKey="revenue" fill="#82ca9d" />
+</BarChart>
         </div>
+
     );
 };
 
 export default Dasboard;
-
-// const Dasboard = () => {
-    
-//     const data = [
-//         {
-//           name: 'Page A',
-//           uv: 4000,
-//           pv: 2400,
-//           amt: 2400,
-//         },
-//         {
-//           name: 'Page B',
-//           uv: 3000,
-//           pv: 1398,
-//           amt: 2210,
-//         },
-//         {
-//           name: 'Page C',
-//           uv: 2000,
-//           pv: 9800,
-//           amt: 2290,
-//         },
-//         {
-//           name: 'Page D',
-//           uv: 2780,
-//           pv: 3908,
-//           amt: 2000,
-//         },
-//         {
-//           name: 'Page E',
-//           uv: 1890,
-//           pv: 4800,
-//           amt: 2181,
-//         },
-//         {
-//           name: 'Page F',
-//           uv: 2390,
-//           pv: 3800,
-//           amt: 2500,
-//         },
-//         {
-//           name: 'Page G',
-//           uv: 3490,
-//           pv: 4300,
-//           amt: 2100,
-//         },
-//       ];
-      
-    
-      
-      
-//           return (
-//             <Dasboard width={400} height={500} data={data}>
-//                  {/* <Line datakey={'uv'}></Line>  */}
-//             </Dasboard>
-         
-//           )
-      
-// };
-
-// export default Dasboard;
